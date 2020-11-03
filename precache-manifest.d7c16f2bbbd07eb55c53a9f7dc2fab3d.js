106f904fdca9bc60be01c573593fa19a7c657718
desmond93 <bondarenkomaxim93@gmail.com>
2020-11-03 20:01:13 +0200

    Updates

---
+++ precache-manifest.d7c16f2bbbd07eb55c53a9f7dc2fab3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54b112602a94937f01e33eb5e0409a37",
+    "url": "/pieChartApp_React/index.html"
+  },
+  {
+    "revision": "77a1dd36147ed6553efa",
+    "url": "/pieChartApp_React/static/css/main.69615851.chunk.css"
+  },
+  {
+    "revision": "3ef128df02b569b5969e",
+    "url": "/pieChartApp_React/static/js/2.a6663522.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pieChartApp_React/static/js/2.a6663522.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77a1dd36147ed6553efa",
+    "url": "/pieChartApp_React/static/js/main.603fe4df.chunk.js"
+  },
+  {
+    "revision": "5d97bcd9462b41c9b1dc",
+    "url": "/pieChartApp_React/static/js/runtime-main.c2489708.js"
+  },
+  {
+    "revision": "70fd4015f6aed6e31e80cfc487e8adb8",
+    "url": "/pieChartApp_React/static/media/18917.70fd4015.ttf"
+  }
+]);
